e2c24675caa004792f3bb702386c86c8155f4247
shuliqi <1169046371@qq.com>
2016-03-04 23:38:57 +0800

    bite

---
+++ bitekaohe/js/index.js
@@ -0,0 +1,36 @@
+function playPause1()
+    { 
+      var myVideo=document.getElementById("slq-video1");
+      if (myVideo.paused) 
+          myVideo.play(); 
+      else 
+          myVideo.pause(); 
+     }
+   function playPause2()
+    { 
+      var myVideo=document.getElementById("slq-video2");
+      if (myVideo.paused) 
+          myVideo.play(); 
+      else 
+          myVideo.pause(); 
+     }
+     function playPause3()
+    { 
+      var myVideo=document.getElementById("slq-video3");
+      if (myVideo.paused) 
+          myVideo.play(); 
+      else 
+          myVideo.pause(); 
+     }
+     function playPause4()
+    { 
+      var myVideo=document.getElementById("slq-video4");
+      if (myVideo.paused) 
+          myVideo.play(); 
+      else 
+          myVideo.pause(); 
+     }
+
+
+
+     
